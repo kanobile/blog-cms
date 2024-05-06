ea1548e7c41c1acd9557445cec70a8f2ef2cf7e1
kanobile <kyuandesign@gmail.com>
2024-05-05 21:29:05 -0400

    auto Blog Posts List

--- index.php
+++ index.php
@@ -2,16 +2,28 @@
     include "header.php";
     include "nav.php";
 ?>
-
+<?php
+    function getPostTitlesFromDatabase() {
+        // TODO in Module 4
+        // get this data from a database instead of hardcoding it
+        $postTitles = array("Blog Post 1", "Blog Post 2", "Blog Post 3");
+        return $postTitles;
+    }
+?>
     <main>
         <h2>Highlights</h2>
         <p>Interesting content in my blog.</p>
         <ul>
-            <li>
-                <a href="post.php">Posts</a>
-            </li>
-            </ul>
-        </main>
+            <? php
+            $postTitles = getPostTitlesFromDatabase() ;
+            foreach ($postTitles as $postTitle) {
+            echo "<li>
+            <a href='post.php?title=" . $postTitle . "'>" . $postTitle . "</a>
+            </li>";
+            }
+            ?>
+        </ul>
+    </main>
 <?php
     include "footer.php";
 ?>
